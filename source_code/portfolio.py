11ac3199c6a2efe2e0f502b488ea716cc9c227dc
Ale-Cas <castricaalessio@gmail.com>
2022-01-03 01:23:41 +0100

    [poetry] fixes

--- source_code/portfolio.py
+++ source_code/portfolio.py
@@ -1,2 +1,17 @@
+"""
+Created on Jan 3, 2022
+@author: AC
+"""
+
+
 class Portfolio:
+    """
+    This class represents Portfolios.
+    """
+
+    def __init__(self) -> None:
+        pass
+
+
+class OptimalPortfolio(Portfolio):
     pass
