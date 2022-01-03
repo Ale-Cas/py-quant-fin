c78669bc755ddb06cdccbec916be26bad4c8717c
Ale-Cas <castricaalessio@gmail.com>
2022-01-03 01:51:14 +0100

    [assets] init

---
+++ source_code/assets.py
@@ -0,0 +1,74 @@
+"""Abstraction layer for Assets."""
+import dataclasses
+from typing import Optional
+
+import pandas as pd
+
+from source_code.portfolio import Portfolio
+
+
+@dataclasses.dataclass
+class Cash:
+    """This class represents Cash."""
+
+    currency: str = "EUR"
+
+
+class Asset:
+    """
+    This class represent a single asset.
+    """
+
+    def __init__(
+        self,
+        name: Optional[str] = None,
+        ticker: Optional[str] = None,
+        isin: Optional[str] = None,  # International Securities Identification Number
+        exchange: Optional[str] = None,
+        asset_class: Optional[str] = None,
+        data_provider: Optional[str] = "Yahoo Finance",
+        prices: pd.DataFrame = pd.DataFrame(),
+    ) -> None:
+        """
+        Initialize the Asset
+        """
+        self.name = name
+        self.ticker = ticker
+        self.isin = isin
+        self.exchange = exchange
+        self.asset_class = asset_class
+        self.data_provider = data_provider
+        self.prices = prices
+
+    def weight_in_ptf(
+        self,
+        portfolio: Portfolio,
+    ) -> float:
+        """Retrieves the weight of the Asset in the specified Portfolio"""
+        return portfolio.holdings[self]
+
+
+class Stock(Asset):
+    """
+    This class represent a single stock.
+    """
+
+    def __init__(
+        self,
+        name: Optional[str] = None,
+        ticker: Optional[str] = None,
+        isin: Optional[str] = None,
+        exchange: Optional[str] = None,
+        data_provider: Optional[str] = "Alpaca",
+    ) -> None:
+        """
+        Initialize the Stock
+        """
+        super().__init__(
+            name,
+            ticker,
+            isin,
+            exchange,
+            asset_class="equity",
+            data_provider=data_provider,
+        )
--- source_code/portfolio.py
+++ source_code/portfolio.py
@@ -2,6 +2,9 @@
 Created on Jan 3, 2022
 @author: AC
 """
+from typing import Dict, Union
+
+from source_code.assets import Asset, Cash
 
 
 class Portfolio:
@@ -9,8 +12,13 @@ class Portfolio:
     This class represents Portfolios.
     """
 
-    def __init__(self) -> None:
-        pass
+    def __init__(
+        self,
+        name: str,  # = "default_portfolio_name",
+        holdings: Dict[Union[Asset, Cash], float],
+    ) -> None:
+        self.name = name
+        self.holdings = holdings or {Cash: 1.0}
 
 
 class OptimalPortfolio(Portfolio):
