6acc0fd4955d8a18ed38f5b6e5d192b25f0c3e9b
Ale-Cas <castricaalessio@gmail.com>
2022-01-14 01:43:30 +0100

    fixes

--- quantfin/portfolio_selection/portfolio_optimization/optimization_problem.py
+++ quantfin/portfolio_selection/portfolio_optimization/optimization_problem.py
@@ -1,6 +1,9 @@
 """Module to implement optimization problems."""
 
+# from cvxopt import matrix, solvers
+
 from quantfin.portfolio_selection.portfolio_optimization import objective_functions
+from quantfin.portfolio_selection import portfolio
 
 
 class OptimizationProblem:
@@ -8,6 +11,10 @@ class OptimizationProblem:
 
     def __init__(
         self,
-        obj_fun: objective_functions.IObjectiveFunction,
+        obj_fun: objective_functions.ObjectiveFunctionType,
     ) -> None:
         self.obj_fun = obj_fun
+
+    def solve(self) -> portfolio.OptimalPortfolio:
+        """Solves the optimization problem."""
+        return portfolio.OptimalPortfolio()
