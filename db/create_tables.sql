6aa35e6c990f9933f67ef4efeba79640e90387db
Ale-Cas <castricaalessio@gmail.com>
2022-01-23 23:00:01 +0100

    fix: .sql

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,28 +1,27 @@
 CREATE TABLE IF NOT EXISTS assets (
-    id SERIAL PRIMARY KEY,
-    ticker TEXT NOT NULL,
-    name TEXT NOT NULL,
-    exchange TEXT NULL,
-    country TEXT NULL,
-    is_etf BOOLEAN NOT NULL,
-    UNIQUE(ticker)
+                            id SERIAL PRIMARY KEY,
+                            ticker TEXT NOT NULL,
+                            name TEXT NOT NULL,
+                            exchange TEXT NULL,
+                            country TEXT NULL,
+                            is_etf BOOLEAN NOT NULL,
+                            UNIQUE(ticker)
 );
 
-
-CREATE TABLE prices (
+CREATE TABLE IF NOT EXISTS prices (
     assets_id INTEGER NOT NULL,
-    time timestamp WITHOUT TIME ZONE NOT NULL,
+    time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     ticker text NULL,
     open double precision NULL,
     low double precision NULL,
     high double precision NULL,
     close double precision NULL,
-    volume int NULL,
+    volume NUMERIC NULL,
     market_cap double precision NULL,
     PRIMARY KEY (assets_id, time),
-    CONSTRAINT fk_assets FOREIGN KEY assets_id REFERENCES assets(id)
+    CONSTRAINT fk_assets FOREIGN KEY (assets_id) REFERENCES assets(id)
 );
-CREATE INDEX ON prices (stock_id, dt DESC);
+CREATE INDEX ON prices (assets_id, "time" DESC);
 CREATE EXTENSION IF NOT EXISTS timescaledb;
 
 SELECT create_hypertable('prices', 'time');
