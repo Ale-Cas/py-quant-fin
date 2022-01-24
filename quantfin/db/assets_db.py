1752ed503dfaaba08da9c0f68bc76a49bc3949bd
Ale-Cas <castricaalessio@gmail.com>
2022-01-24 18:50:45 +0100

    feat db

---
+++ quantfin/db/assets_db.py
@@ -0,0 +1,50 @@
+"""This module implements tools to interact with TimescaleDB."""
+from abc import ABC, abstractmethod
+from typing import Optional
+
+import config
+
+
+class DataBase(ABC):
+    """This abstract class implements tools to interact with a database."""
+
+    def __init__(
+        self,
+        host: str,
+        name: str,
+        user: str,
+        password: str,
+        db_type: str,
+    ) -> None:
+        self.host = host
+        self.name = name
+        self.user = user
+        self.password = password
+        self.db_type = db_type
+
+    @abstractmethod
+    def connect(self) -> None:
+        pass
+
+    @abstractmethod
+    def close_connection(self) -> None:
+        pass
+
+
+class TimescaleDB(DataBase):
+    """This class implements tools to interact with TimescaleDB."""
+
+    def __init__(
+        self,
+        host: str = config.DB_HOST,
+        name: str = config.DB_NAME,
+        user: str = config.DB_USER,
+        password: str = config.DB_PASSWORD,
+    ) -> None:
+        super().__init__(
+            host,
+            name,
+            user,
+            password,
+            db_type="TimescaleDB",
+        )
